694932fb3131b6c8fa212ef6f6bb3ce71f6addae
Allen <646149636@qq.com>
2019-02-01 16:46:57 +0800

    初始化。

---
+++ CheckLists/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CheckLists
+//
+//  Created by Allen on 2019/1/28.
+//  Copyright © 2019 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ CheckLists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheckLists
+//
+//  Created by Allen on 2019/1/28.
+//  Copyright © 2019 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
